5955a9363ed4369173f9db1a843fda77e75ef615
djlincero <djlincero@gmail.com>
2026-07-28 09:04:41 -0400

    Complete professionnal portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://djlincero.github.io',
+  base: '/portfolio',
+
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
